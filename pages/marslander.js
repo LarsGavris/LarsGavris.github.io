c44de288e44cafdbdb74ea5af86be992dce55680
LarsGavris <lars.gavris@student.kit.edu>
2021-07-30 22:10:47 +0200

    fixed js

--- pages/marslander.js
+++ pages/marslander.js
@@ -1,3 +1,8 @@
 console.log("hello world!");
 
-let screen = document.getElementById("canvas");
+let canvas = document.getElementById("marslander-canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = rgba(1,1,1,1);
+ctx.fillRect(0,0,100,100);
+
